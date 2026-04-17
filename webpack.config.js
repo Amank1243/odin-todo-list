61571638aea019e5e0ab68212bcd266bee8d3501
Amank1243 <148727967+Amank1243@users.noreply.github.com>
2026-04-16 17:37:09 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+import path from "node:path"
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "deployment",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      favicon: "./images/beach.png",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+      test: /\.(png|svg|jpe?g|gif)$/i,
+      type: "asset/resource",
+      },
+    ],
+  },
+};
