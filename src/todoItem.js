6ce272d86fc74667438ba5a577ba55c6f87c5e0b
Aman Khajuria <24khajuriaa@gmail.com>
2026-04-26 17:44:11 -0700

    Some basic work

---
+++ src/todoItem.js
@@ -0,0 +1,17 @@
+
+function todoItem() {
+    let title = ' ';
+    let description = ' ';
+
+    let date = new Date();
+    let dateCreated = date.getMonth() + "/" + date.getDate() + '/' + date.getFullYear();
+     
+    let dueDate = ' ';
+    let priority = ' '; // Maybe make this into an enum?
+    
+
+};
+
+let date = new Date()
+console.log(date.getMonth() + "/" + date.getDate() + '/' + date.getFullYear());
+
