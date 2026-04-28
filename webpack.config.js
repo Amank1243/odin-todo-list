f5f259a33acd2c68a4cd9a38463ab5f4449b9ff8
Aman Khajuria <24khajuriaa@gmail.com>
2026-04-27 19:14:56 -0700

    Added css loader package

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,7 @@ export default {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
-      favicon: "",
+      template: "./src/template.html"
     }),
   ],
   module: {
